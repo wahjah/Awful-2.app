440c1eb6e554806333b4d052a42449acf2964813
Nolan Waite <nolan@nolanw.ca>
2013-08-27 02:29:44 -0300

    Remove dependency on fishhook.

--- Code/QuietAssertMacros.c
+++ Code/QuietAssertMacros.c
@@ -5,27 +5,25 @@
 //  Created by Nolan Waite on 2013-08-10.
 //
 
-// Xcode5-DP5 spams the console like crazy. This is a workaround.
-// https://devforums.apple.com/message/861797 see post #11 by 0xced
+// Xcode5-DP5 and DP6 spam the console like crazy. This is a workaround.
+// https://gist.github.com/OdNairy/6219906
 
-#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-#include "fishhook.h"
 
-int quiet_fprintf(FILE * restrict stream, const char * restrict format, ...)
+typedef int (*writer)(void *, const char *, int);
+static writer _oldWriter;
+
+static int quiet_writer(void *fd, const char *buffer, int size)
 {
-    if (strncmp(format, "AssertMacros:", 13) == 0) {
-        return 0;
+    if (strncmp(buffer, "AssertMacros: queueEntry", 24) == 0) {
+        return size;
     }
-    va_list args;
-    va_start(args, format);
-    int result = vfprintf(stream, format, args);
-    va_end(args);
-    return result;
+    return _oldWriter(fd, buffer, size);
 }
 
 __attribute__((constructor)) void QuietAssertMacros(void)
 {
-    rebind_symbols((struct rebinding[1]){{ "fprintf", quiet_fprintf }}, 1);
+    _oldWriter = stderr->_write;
+    stderr->_write = quiet_writer;
 }
