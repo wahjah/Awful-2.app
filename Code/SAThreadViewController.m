ba281cb22d9f7cce166043694e4160f9b825a562
Nolan Waite <nolan@nolanw.ca>
2013-08-28 07:10:08 -0300

    Show authors in posts view and clean it up.
    The key here is moving each post into its own section in the collection view.
    This allows the use of the flow layout's header view (for the author's name),
    footer view (for a simple divider), and sectionInset (instead of repeating an
    inset in each item's constraints and the item layout). Using the footer view in
    turn allows the collection view's background color to be set properly so the
    navigation bar doesn't look weird.

--- Code/SAThreadViewController.m
+++ Code/SAThreadViewController.m
@@ -8,9 +8,19 @@
 #import "SAThreadViewController.h"
 #import "SAForumsClient.h"
 
+@interface SAPostHeaderView : UICollectionReusableView
+
+@property (readonly, strong, nonatomic) UILabel *authorUsernameLabel;
+
+@end
+
 @interface SAPostColletionViewCell : UICollectionViewCell
 
 @property (readonly, strong, nonatomic) UITextView *textView;
+
+@end
+
+@interface SAPostDividerView : UICollectionReusableView
 
 @end
 
@@ -25,8 +35,10 @@
 - (id)initWithThread:(SAThread *)thread
 {
     UICollectionViewFlowLayout *layout = [UICollectionViewFlowLayout new];
-    layout.minimumLineSpacing = 10;
+    layout.minimumLineSpacing = 0;
     layout.minimumInteritemSpacing = 0;
+    layout.sectionInset = UIEdgeInsetsMake(5, 5, 5, 5);
+    layout.headerReferenceSize = CGSizeMake(0, 54);
     self = [super initWithCollectionViewLayout:layout];
     if (self) {
         _thread = thread;
@@ -37,8 +49,14 @@
 - (void)loadView
 {
     [super loadView];
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    [self.collectionView registerClass:[SAPostHeaderView class]
+            forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
+                   withReuseIdentifier:@"Header"];
     [self.collectionView registerClass:[SAPostColletionViewCell class] forCellWithReuseIdentifier:@"Post"];
+    [self.collectionView registerClass:[SAPostDividerView class]
+            forSupplementaryViewOfKind:UICollectionElementKindSectionFooter
+                   withReuseIdentifier:@"Divider"];
     __weak __typeof__(self) weakSelf = self;
     [self.thread postsOnPage:1 completionHandler:^(NSError *error, NSArray *posts) {
         __typeof__(self) strongSelf = weakSelf;
@@ -49,11 +67,16 @@
     }];
 }
 
-#pragma mark UICollectionViewDataSource and UICollectionViewDelegate
+#pragma mark UICollectionViewDataSource and UICollectionViewDelegateFlowLayout
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return self.posts.count;
+}
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.posts.count;
+    return 1;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
@@ -61,26 +84,80 @@
 {
     SAPostColletionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Post"
                                                                               forIndexPath:indexPath];
-    SAPost *post = self.posts[indexPath.item];
+    SAPost *post = self.posts[indexPath.section];
     cell.textView.attributedText = post.stringContents;
     return cell;
 }
 
-static const UIEdgeInsets TextViewInsets = (UIEdgeInsets){ .left = 5, .right = 5 };
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView
+           viewForSupplementaryElementOfKind:(NSString *)kind
+                                 atIndexPath:(NSIndexPath *)indexPath
+{
+    if ([kind isEqualToString:UICollectionElementKindSectionHeader]) {
+        SAPostHeaderView *header = [collectionView dequeueReusableSupplementaryViewOfKind:kind
+                                                                      withReuseIdentifier:@"Header"
+                                                                             forIndexPath:indexPath];
+        SAPost *post = self.posts[indexPath.section];
+        header.authorUsernameLabel.text = post.author.username;
+        return header;
+    }
+    else if ([kind isEqualToString:UICollectionElementKindSectionFooter]) {
+        return [collectionView dequeueReusableSupplementaryViewOfKind:kind
+                                                  withReuseIdentifier:@"Divider"
+                                                         forIndexPath:indexPath];
+        
+    }
+    else {
+        return nil;
+    }
+}
 
 - (CGSize)collectionView:(UICollectionView *)collectionView
                   layout:(UICollectionViewLayout *)collectionViewLayout
   sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    SAPost *post = self.posts[indexPath.item];
-    CGFloat textViewWidth = CGRectGetWidth(UIEdgeInsetsInsetRect(collectionView.frame, TextViewInsets));
-    CGRect textBounds = [post.stringContents boundingRectWithSize:CGSizeMake(textViewWidth, 0)
+    SAPost *post = self.posts[indexPath.section];
+    UIEdgeInsets sectionInset = ((UICollectionViewFlowLayout *)collectionViewLayout).sectionInset;
+    CGFloat itemWidth = CGRectGetWidth(UIEdgeInsetsInsetRect(collectionView.frame, sectionInset));
+    CGRect textBounds = [post.stringContents boundingRectWithSize:CGSizeMake(itemWidth, 0)
                                                           options:(NSStringDrawingUsesLineFragmentOrigin |
                                                                    NSStringDrawingUsesFontLeading)
                                                           context:nil];
     
     // Without the `+ 1` the last line is typically cut off when the UITextView renders. Worth further investigation, but this works for now.
-    return CGSizeMake(CGRectGetWidth(collectionView.frame), ceilf(CGRectGetHeight(textBounds)) + 1);
+    return CGSizeMake(itemWidth, ceilf(CGRectGetHeight(textBounds)) + 1);
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+referenceSizeForFooterInSection:(NSInteger)section
+{
+    if (section + 1 == collectionView.numberOfSections) {
+        return CGSizeZero;
+    }
+    return CGSizeMake(0, 1);
+}
+
+@end
+
+@implementation SAPostHeaderView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _authorUsernameLabel = [UILabel new];
+        _authorUsernameLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        _authorUsernameLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+        [self addSubview:_authorUsernameLabel];
+        NSDictionary *views = @{ @"username": _authorUsernameLabel };
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[username]|"
+                                                                     options:0
+                                                                     metrics:nil
+                                                                       views:views]];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[username]|" options:0 metrics:nil views:views]];
+    }
+    return self;
 }
 
 @end
@@ -91,6 +168,8 @@ static const UIEdgeInsets TextViewInsets = (UIEdgeInsets){ .left = 5, .right = 5
 {
     self = [super initWithFrame:frame];
     if (self) {
+        self.contentView.backgroundColor = [UIColor whiteColor];
+        
         _textView = [[UITextView alloc] initWithFrame:(CGRect){ .size = frame.size }];
         _textView.translatesAutoresizingMaskIntoConstraints = NO;
         _textView.editable = NO;
@@ -102,15 +181,27 @@ static const UIEdgeInsets TextViewInsets = (UIEdgeInsets){ .left = 5, .right = 5
         
         [self.contentView addSubview:_textView];
         NSDictionary *views = @{ @"textView": _textView };
-        NSDictionary *metrics = @{ @"left": @(TextViewInsets.left), @"right": @(TextViewInsets.right) };
-        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-left-[textView]-right-|"
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[textView]|"
                                                                                  options:0
-                                                                                 metrics:metrics
+                                                                                 metrics:nil
                                                                                    views:views]];
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[textView]|"
                                                                                  options:0
                                                                                  metrics:nil
                                                                                    views:views]];
+    }
+    return self;
+}
+
+@end
+
+@implementation SAPostDividerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor lightGrayColor];
     }
     return self;
 }
